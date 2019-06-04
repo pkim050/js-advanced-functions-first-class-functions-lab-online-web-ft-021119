a4d8565f24b6cba9ef65d09cf8fc4762f46bc4d1
Patrick Kim <pkim050@gmail.com>
2019-06-04 04:40:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 cosnt returnFirstTwoDrivers = function (array) {
   const temp = function () {
     const temp2 = array.slice(0, 2);
-    console.log(temp2);
+    console.log(array);
     return temp2;
   }
 }
