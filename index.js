5e448dc9436df53f67e6e74499c6b638e41816eb
Patrick Kim <pkim050@gmail.com>
2019-06-04 05:09:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,4 @@ const returnLastTwoDrivers = function(array) {
   return array.slice(-2);
 };
 
-let selectingDrivers = ["", ""];
-selectingDrivers[0] = returnFirstTwoDrivers(array);
-selectingDrivers[1] = returnLastTwoDrivers(array);
-selectingDrivers;
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
