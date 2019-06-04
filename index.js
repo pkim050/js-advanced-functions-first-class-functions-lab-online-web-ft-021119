01df4981d700981764d762bc42e9cc740d35cc9e
Patrick Kim <pkim050@gmail.com>
2019-06-04 04:42:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Code your solution in this file!
 function returnFirstTwoDrivers(array) {
   const temp = function () {
+    console.log(array);
     const temp2 = array.slice(0, 2);
     return temp2;
   }
